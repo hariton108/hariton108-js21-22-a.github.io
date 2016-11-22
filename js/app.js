53e42d7e6aa0cbe4d76e2c447ad9a6c9ce761c08
hariton108 <hariton108@gmail.com>
2016-11-22 15:48:25 +0200

    First version

---
+++ js/app.js
@@ -0,0 +1,44 @@
+function calcExp(base, exp) {
+
+	var res = 1;
+	var result;
+
+	for (var i = 0; i < Math.abs(exp); i++) {
+		
+		res *= base;	
+	}
+
+	if (exp >= 0) {
+
+		result = res;
+
+		console.log(result);
+
+		return result;
+
+	} else result = 1/res;
+	
+	console.log(result);
+
+	return result;		
+};
+
+function startCalcExp(base, exp) {
+
+	var base = prompt('Введите целое число', '');
+
+	if ((base != null) && ((base ^ 0) == base) && (base != NaN) && (base != '')) {
+
+		var exp = prompt('Введите степень (целое число)', '');
+		
+		if ((exp != null) && ((exp ^ 0) == exp) && (exp != NaN) && (exp != '')) {
+
+			calcExp(base, exp);
+		}
+
+	} else alert('Попробуйте еще раз');
+
+	return false;
+};
+
+startCalcExp();
---
+++ spec/spec.js
@@ -0,0 +1,32 @@
+describe("calcExp", function() {
+
+	it("Функция определена", function() {
+
+		expect(calcExp).toBeDefined();
+	});
+
+	it("Возводит в положительную степень", function() {
+
+		expect(calcExp(5, 2)).toEqual(25);
+	});
+
+	it("Возводит в отрицательную степень", function() {
+
+		expect(calcExp(5, -2)).toEqual(0.04);
+	});
+
+	it("Возводит в нулевую степень", function() {
+
+		expect(calcExp(5, 0)).toEqual(1);
+	});
+
+});
+
+describe("startCalcExp", function() {
+
+	it("Функция определена", function() {
+
+		expect(startCalcExp).toBeDefined();
+	});
+
+});
